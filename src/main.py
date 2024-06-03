f71e796a51375fb69a4d02b0d2373ee8b03eb866
olofdahlman <olofdahlman@live.se>
2024-06-03 16:52:49 +0200

    4: Added sh running script, src folder with main.py for main operations and textnode document with text class and related methods

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# Use ./main.sh to run from terminal
+print("hello world")
+from textnode import TextNode
+
+def main():
+    testtext = TextNode("I am inserting generic sample text here","italic","https://www.boot.dev")
+    print(testtext)
+
+if __name__ == "__main__":
+    main()
+#main()
